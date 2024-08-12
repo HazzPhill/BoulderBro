c441273e920d085b2fa0106ea92916ff53b08e9a
HazzPhill <hazphillips@outlook.com>
2024-08-12 17:29:08 +0100

    Initial Commit

---
+++ BoulderBro/BoulderBroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoulderBroApp.swift
+//  BoulderBro
+//
+//  Created by Hazz on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BoulderBroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
