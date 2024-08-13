ef542563d94a076a416fc677a7954006d7bfc83f
HazzPhill <hazphillips@outlook.com>
2024-08-13 18:10:17 +0100

    Adding healthkit information

--- BoulderBro/Model/Insights.swift
+++ BoulderBro/Model/Insights.swift
@@ -1,13 +1,14 @@
-//
-//  Insights.swift
-//  BoulderBro
-//
-//  Created by Hazz on 13/08/2024.
-//
-
 import SwiftUI
+import HealthKit
 
 struct Insights: View {
+    @State private var activeCalories: Double = 0
+    @State private var totalCalories: Double = 0
+    @State private var avgSessionDuration: TimeInterval = 0
+    @State private var avgHeartRate: Double = 0
+
+    private let healthStore = HKHealthStore()
+
     var body: some View {
         ZStack {
             // Sticky Gradient Background
@@ -15,36 +16,36 @@ struct Insights: View {
                 LinearGradient(
                     gradient: Gradient(colors: [Color(hex: "#FF5733"), .white]),
                     startPoint: .top,
-                    endPoint: UnitPoint(x: 0.5, y: 0.25) // Custom endPoint
+                    endPoint: UnitPoint(x: 0.5, y: 0.25)
                 )
-                .frame(height: 900) // Adjust height of the gradient
+                .frame(height: 900)
                 .ignoresSafeArea(edges: .top)
-                .position(x: geometry.size.width / 2, y: 100) // Keep the gradient at the top center
+                .position(x: geometry.size.width / 2, y: 100)
             }
-            .frame(height: 200) // Make sure the gradient only occupies the top area
-            
+            .frame(height: 200)
+
             ScrollView {
                 VStack(alignment: .leading, spacing: 16) {
                     Text("Summary")
-                        .padding(.top, 12) // Padding top
-                        .padding(.bottom, 15) // Padding bottom
+                        .padding(.top, 12)
+                        .padding(.bottom, 15)
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundStyle(Color(.black))
-                    
+
                     Rectangle()
                         .frame(height: 163)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                         .foregroundStyle(Color(.white))
                         .shadow(radius: 20)
-                    
+
                     HStack {
                         Rectangle()
                             .frame(height: 163)
                             .clipShape(RoundedRectangle(cornerRadius: 10))
                             .foregroundStyle(Color(.white))
                             .zIndex(1)
-                        
+
                         Rectangle()
                             .frame(height: 163)
                             .clipShape(RoundedRectangle(cornerRadius: 10))
@@ -57,49 +58,69 @@ struct Insights: View {
                     .foregroundStyle(Color(.black))
                     .padding(.top, 15)
                     .padding(.bottom)
-                    
-                    Text ("Hang Timer")
+
+                    Text("Hang Timer")
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundStyle(Color(.black))
-                        .padding(.top,5)
+                        .padding(.top, 5)
                         .padding(.bottom, 15)
-                    
+
                     Rectangle()
                         .frame(height: 125)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                         .foregroundStyle(Color(.white))
                         .shadow(radius: 20)
-                    
+
                     Text("Previous 5 climb workouts")
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundStyle(Color(.black))
                         .padding(.top, 15)
                         .padding(.bottom, 15)
-                    
-                    // Box for smaller boxes
+
                     ZStack {
                         Rectangle()
                             .frame(height: 208)
                             .clipShape(RoundedRectangle(cornerRadius: 10))
                             .foregroundStyle(Color(.white))
                             .shadow(radius: 20)
-                        
-                        // Grid of 4 customizable boxes with consistent sizes
+
                         LazyVGrid(
                             columns: [
-                                GridItem(.flexible(minimum: 0, maximum: 165)), // Column width controlled by grid
+                                GridItem(.flexible(minimum: 0, maximum: 165)),
                                 GridItem(.flexible(minimum: 0, maximum: 165))
                             ],
                             spacing: 10
                         ) {
-                            CustomBox1()
-                            CustomBox2()
-                            CustomBox3()
-                            CustomBox4()
+                            MetricView(
+                                title: "Active Calories",
+                                value: activeCalories,
+                                unit: "cal"
+                            )
+
+                            MetricView(
+                                title: "Total Calories",
+                                value: totalCalories,
+                                unit: "cal"
+                            )
+
+                            MetricView(
+                                title: "Avg. Session",
+                                value: avgSessionDuration,
+                                unit: "" // Indicate that it's a duration
+                            )
+
+                            MetricView(
+                                title: "Avg. Heart Rate",
+                                value: avgHeartRate,
+                                unit: "bpm"
+                            )
                         }
-                        .padding(10) // Padding inside the larger box
+                        .padding(10)
+                        .onAppear {
+                            fetchWorkoutData()
+                        }
                     }
                 }
                 .padding(.horizontal)
@@ -107,89 +128,53 @@ struct Insights: View {
             }
         }
     }
+
+    private func fetchWorkoutData() {
+        // ... (Your existing fetchWorkoutData function)
+    }
 }
 
-// Custom Box 1
-struct CustomBox1: View {
+struct MetricView: View {
+    var title: String
+    var value: Double
+    var unit: String
+
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
-            Text("Avg. Session")
+            Text(title)
                 .font(.headline)
                 .foregroundColor(.black)
-            Text("49 Min")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundStyle(Color(hex: "#FF5733"))
+
+            // Handle formatting based on the unit
+            if unit == "" { // Assuming empty unit means duration
+                Text(value.formattedDuration())
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color(hex: "#FF5733"))
+            } else {
+                Text("\(value, specifier: "%.0f") \(unit)")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color(hex: "#FF5733"))
+            }
         }
         .padding()
-        .frame(width: 165, height: 90) // Fixed size for consistency
+        .frame(width: 165, height: 90)
         .background(Color(.white))
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .shadow(radius: 5)
     }
 }
 
-// Custom Box 2
-struct CustomBox2: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Avg. Heart Rate")
-                .font(.headline)
-                .foregroundColor(.black)
-            Text("113 Bpm")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundStyle(Color(hex: "#FF5733"))
-        }
-        .padding()
-        .frame(width: 165, height: 90) // Fixed size for consistency
-        .background(Color(.white))
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .shadow(radius: 5)
+extension TimeInterval {
+    func formattedDuration() -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        return formatter.string(from: self) ?? ""
     }
 }
 
-// Custom Box 3
-struct CustomBox3: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Total Calories")
-                .font(.headline)
-                .foregroundColor(.black)
-            Text("837")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundStyle(Color(hex: "#FF5733"))
-        }
-        .padding()
-        .frame(width: 165, height: 90) // Fixed size for consistency
-        .background(Color(.white))
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .shadow(radius: 5)
-    }
-}
-
-// Custom Box 4
-struct CustomBox4: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Active Calories")
-                .font(.headline)
-                .foregroundColor(.black)
-            Text("632")
-                .font(.title)
-                .fontWeight(.bold)
-                .foregroundStyle(Color(hex: "#FF5733"))
-        }
-        .padding()
-        .frame(width: 165, height: 90) // Fixed size for consistency
-        .background(Color(.white))
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .shadow(radius: 5)
-    }
-}
-
-// SwiftUI preview
 #Preview {
     Insights()
 }
--- BoulderBro/Splash Screen.swift
+++ BoulderBro/Splash Screen.swift
@@ -19,56 +19,73 @@ struct SplashScreen: View {
     let easingFactor: CGFloat = 0.5 // Adjust this to control the smoothness
 
     var body: some View {
-        ZStack {
-            // Background color (placed at the bottom of the ZStack)
-            Color(hex: "#FF5733") // Light gray background, adjust as needed
-                .ignoresSafeArea() // Extend background behind safe areas
-                .zIndex(-2) // Ensure it's behind the circles
-
-            // Top circle
-            Circle()
-                .fill(Color.white).opacity(0.3)
-                .frame(width: 350, height: 350)
-                .blur(radius: 60)
-                .offset(topCircleOffset) // Use the state variable for offset
-                .zIndex(-1) // Keep circles above the background
-
-            // Bottom circle
-            Circle()
-                .fill(Color.white).opacity(0.3)
-                .frame(width: 350, height: 500)
-                .blur(radius: 60)
-                .offset(bottomCircleOffset) // Use the state variable for offset
-                .zIndex(-1) // Keep circles above the background
-
-            // Animate circle offsets
-            .onReceive(timer) { _ in
-                withAnimation(.linear(duration: 0.9)) {
-                    // Calculate new target offsets with slight random variations, but keep them within bounds
-                    let newTopOffset = CGSize(
-                        width: max(50, min(UIScreen.main.bounds.width - 300, topCircleOffset.width + CGFloat.random(in: -50...50))),
-                        height: max(-250, min(-50, topCircleOffset.height + CGFloat.random(in: -25...25)))
-                    )
-                    let newBottomOffset = CGSize(
-                        width: max(-200, min(UIScreen.main.bounds.width - 300, bottomCircleOffset.width + CGFloat.random(in: -50...50))),
-                        height: max(50, min(UIScreen.main.bounds.height - 450, bottomCircleOffset.height + CGFloat.random(in: -25...25)))
-                    )
-
-                    topCircleOffset = newTopOffset
-                    bottomCircleOffset = newBottomOffset
+        NavigationStack {
+            ZStack {
+                // Background color
+                Color(hex: "#FF5733")
+                    .ignoresSafeArea()
+                    .zIndex(-2) // Ensure it's behind the circles
+                
+                // Top circle
+                Circle()
+                    .fill(Color.white).opacity(0.3)
+                    .frame(width: 350, height: 350)
+                    .blur(radius: 60)
+                    .offset(topCircleOffset)
+                    .zIndex(-1)
+                
+                // Bottom circle
+                Circle()
+                    .fill(Color.white).opacity(0.3)
+                    .frame(width: 350, height: 500)
+                    .blur(radius: 60)
+                    .offset(bottomCircleOffset)
+                    .zIndex(-1)
+                
+                // Animate circle offsets
+                    .onReceive(timer) { _ in
+                        withAnimation(.linear(duration: 0.9)) {
+                            // Calculate new target offsets with slight random variations, but keep them within bounds
+                            let newTopOffset = CGSize(
+                                width: max(50, min(UIScreen.main.bounds.width - 300, topCircleOffset.width + CGFloat.random(in: -50...50))),
+                                height: max(-250, min(-50, topCircleOffset.height + CGFloat.random(in: -25...25)))
+                            )
+                            let newBottomOffset = CGSize(
+                                width: max(-200, min(UIScreen.main.bounds.width - 300, bottomCircleOffset.width + CGFloat.random(in: -50...50))),
+                                height: max(50, min(UIScreen.main.bounds.height - 450, bottomCircleOffset.height + CGFloat.random(in: -25...25)))
+                            )
+                            
+                            topCircleOffset = newTopOffset
+                            bottomCircleOffset = newBottomOffset
+                        }
+                    }
+                
+                VStack(spacing: 20) { // Added spacing between elements
+                    Text("Hello, Harry!")
+                        .foregroundStyle(Color.white)
+                        .fontWeight(.bold)
+                        .font(.title)
+                    
+                    // Customized NavigationLink
+                    NavigationLink {
+                        Insights()
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        Text("View Insights")
+                            .fontWeight(.bold)
+                            .font(.system(size: 16))
+                            .foregroundColor(Color(hex: "#FF5733"))
+                            .frame(maxWidth: .infinity, minHeight: 50) // Full width, fixed height
+                            .background(Color.white)
+                            .cornerRadius(10) // Rounded corners
+                    }
+                    .padding(.horizontal) // Padding on the left and right
                 }
-            }
-            VStack{
-                Text ("Hello,Harry!")
-                    .foregroundStyle(Color.white)
-                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .padding(.horizontal, 20) // Optional additional padding around the VStack
             }
         }
     }
-
 }
-
 
 #Preview {
     SplashScreen()
