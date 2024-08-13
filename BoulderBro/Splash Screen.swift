7a1b2de45780641d67a676065eafd7164b3186b2
HazzPhill <hazphillips@outlook.com>
2024-08-13 09:38:56 +0100

    Insights page build

---
+++ BoulderBro/Splash Screen.swift
@@ -0,0 +1,18 @@
+//
+//  Splash Screen.swift
+//  BoulderBro
+//
+//  Created by Hazz on 12/08/2024.
+//
+
+import SwiftUI
+
+struct Splash_Screen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splash_Screen()
+}
