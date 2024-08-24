8b08a5aa6b532b824e7d4ccf60076eaa443c7f52
HazzPhill <hazphillips@outlook.com>
2024-08-24 13:43:31 +0100

    MEMBERSHIP CARD STYLING

--- BoulderBro/Model/MembershipCardView.swift
+++ BoulderBro/Model/MembershipCardView.swift
@@ -17,10 +17,10 @@ struct ManualCropView: UIViewControllerRepresentable {
         config.cropShapeType = .rect
         let cropViewController = Mantis.cropViewController(image: image!, config: config)
         cropViewController.delegate = context.coordinator
-        
+
         let navigationController = UINavigationController(rootViewController: cropViewController)
         navigationController.modalPresentationStyle = .fullScreen
-        
+
         return navigationController
     }
 
@@ -60,126 +60,165 @@ struct MembershipCardView: View {
     @State private var membershipImage: UIImage? = nil
     @State private var croppedImage: UIImage? = nil
     @State private var cardInfo: String = ""
+    @State private var gymName: String = ""
     @State private var showImagePicker = false
     @State private var showRemoveAlert = false
     @State private var showSaveAlert = false
     @State private var alertMessage = ""
     @State private var isManualCropping = false
-
+    @State private var showEditOptions = false
+    @State private var isEditingInfo = false
+    @EnvironmentObject var viewModel: AuthViewModel
+    @Environment(\.colorScheme) var colorScheme
     @Environment(\.presentationMode) var presentationMode
+    
+    // Circle variables for animated background circles
+    @State private var topCircleOffset = CGSize(width: 150, height: -300)
+    @State private var bottomCircleOffset = CGSize(width: -150, height: 250)
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
 
     var body: some View {
-        VStack {
-            if let image = croppedImage ?? membershipImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity, maxHeight: 300)
-                    .padding()
-                    .background(Color("CardBackground"))
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-            } else {
-                Text("No Membership Card Uploaded")
-                    .foregroundColor(.gray)
-                    .padding()
-            }
+        if let user = viewModel.currentUser {
+            ZStack {
+                // Background color
+                Color(colorScheme == .dark ? Color(hex: "#1f1f1f") : Color(hex: "#f1f0f5"))
+                    .ignoresSafeArea()
+                    .zIndex(-2)
 
-            Text(cardInfo)
-                .font(.custom("YourCustomFont", size: 16))
-                .padding()
+                // Top circle
+                Circle()
+                    .fill(Color(hex: "#FF5733")).opacity(0.3)
+                    .frame(width: 350, height: 350)
+                    .blur(radius: 60)
+                    .offset(topCircleOffset)
+                    .opacity(0.5)
+                    .zIndex(-1)
 
-            HStack {
-                Button(action: {
-                    showImagePicker.toggle()
-                }) {
-                    Text("Update Card")
-                        .font(.custom("YourCustomFont-Bold", size: 16))
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.accentColor)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding()
+                // Bottom circle
+                Circle()
+                    .fill(Color(hex: "#FF5733")).opacity(0.3)
+                    .frame(width: 350, height: 500)
+                    .blur(radius: 60)
+                    .offset(bottomCircleOffset)
+                    .opacity(0.5)
+                    .zIndex(-1)
 
-                if membershipImage != nil || croppedImage != nil {
-                    Button(action: {
-                        showRemoveAlert.toggle()
-                    }) {
-                        Text("Remove Card")
-                            .font(.custom("YourCustomFont-Bold", size: 16))
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.red.opacity(0.7))
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                    .padding()
-                    .alert(isPresented: $showRemoveAlert) {
-                        Alert(
-                            title: Text("Remove Membership Card"),
-                            message: Text("Are you sure you want to remove your membership card?"),
-                            primaryButton: .destructive(Text("Remove")) {
-                                membershipImage = nil
-                                croppedImage = nil
-                                cardInfo = ""
-                                removeMembershipCardFromFirestore()
-                            },
-                            secondaryButton: .cancel()
+                // Animate circle offsets
+                .onReceive(timer) { _ in
+                    withAnimation(.linear(duration: 0.9)) {
+                        let newTopOffset = CGSize(
+                            width: max(50, min(UIScreen.main.bounds.width - 300, topCircleOffset.width + CGFloat.random(in: -50...50))),
+                            height: max(-250, min(-50, topCircleOffset.height + CGFloat.random(in: -25...25)))
                         )
+                        let newBottomOffset = CGSize(
+                            width: max(-200, min(UIScreen.main.bounds.width - 300, bottomCircleOffset.width + CGFloat.random(in: -50...50))),
+                            height: max(50, min(UIScreen.main.bounds.height - 450, bottomCircleOffset.height + CGFloat.random(in: -25...25)))
+                        )
+
+                        topCircleOffset = newTopOffset
+                        bottomCircleOffset = newBottomOffset
                     }
                 }
-            }
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 16) {
+                        Text ("\(user.firstName)'s Membership Card")
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.5)
+                            .font(.custom("Kurdis-ExtraWideBlack", size: 30))
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color(colorScheme == .dark ? Color(hex: "#ffffff") : Color(hex: "#000000")))
+                            .opacity(0.7)
+                            .padding(.top)
 
-            if membershipImage != nil || croppedImage != nil {
-                Button(action: {
-                    isManualCropping = true
-                }) {
-                    Text("Manual Crop")
-                        .font(.custom("YourCustomFont-Bold", size: 16))
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding()
-            }
+                        if let image = croppedImage ?? membershipImage {
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: .infinity, maxHeight: 300)
+                                .padding()
+                                .background(Color(colorScheme == .dark ? Color(hex: "#333333") : .white))
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                            
+                            VStack(spacing: 16) { // Stack buttons vertically
+                                EditButton(showEditOptions: $showEditOptions)
+                                    .frame(maxWidth: .infinity) // Ensure buttons are the same width
+                                
+                                if shouldShowSaveButton {
+                                    SaveButton(action: saveToFirebase)
+                                        .frame(maxWidth: .infinity)
+                                        .alert(isPresented: $showSaveAlert) {
+                                            Alert(title: Text(alertMessage))
+                                        }
+                                }
+                            }
+                            .padding(.horizontal)
+                            .actionSheet(isPresented: $showEditOptions) {
+                                ActionSheet(
+                                    title: Text("Edit Membership Card"),
+                                    buttons: editActionButtons()
+                                )
+                            }
+                            .alert(isPresented: $showRemoveAlert) {
+                                removeAlert()
+                            }
+                        } else {
+                            EmptyStateView(showImagePicker: $showImagePicker)
+                                .frame(maxWidth: .infinity)
+                        }
 
-            if (membershipImage != nil || croppedImage != nil) && !cardInfo.isEmpty {
-                Button(action: {
-                    saveToFirebase()
-                }) {
-                    Text("Save")
-                        .font(.custom("YourCustomFont-Bold", size: 16))
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding()
-                .alert(isPresented: $showSaveAlert) {
-                    Alert(title: Text(alertMessage))
+                        if shouldShowCardInfo {
+                            CardInfoView(cardInfo: $cardInfo, gymName: $gymName, isEditingInfo: $isEditingInfo)
+                                .frame(maxWidth: .infinity)
+                        }
+                    }
+                    .padding()
                 }
             }
-        }
-        .padding()
-        .background(Color("Background"))
-        .cornerRadius(15)
-        .shadow(radius: 10)
-        .sheet(isPresented: $showImagePicker, onDismiss: processImage) {
-            ImagePicker(selectedImage: $membershipImage, selectedVideoURL: .constant(nil))
-        }
-        .fullScreenCover(isPresented: $isManualCropping) {
-            ManualCropView(image: $membershipImage, isPresented: $isManualCropping) { croppedImage in
-                self.croppedImage = croppedImage
-                extractText(from: croppedImage)
+            .sheet(isPresented: $showImagePicker, onDismiss: processImage) {
+                ImagePicker(selectedImage: $membershipImage, selectedVideoURL: .constant(nil))
             }
+            .fullScreenCover(isPresented: $isManualCropping) {
+                ManualCropView(image: $membershipImage, isPresented: $isManualCropping) { croppedImage in
+                    self.croppedImage = croppedImage
+                    extractText(from: croppedImage)
+                }
+            }
+            .onAppear(perform: loadMembershipCardData)
         }
-        .navigationTitle("Membership Card")
-        .onAppear(perform: loadMembershipCardData)
+    }
+
+    private var shouldShowCardInfo: Bool {
+        !cardInfo.isEmpty || !gymName.isEmpty || membershipImage != nil || croppedImage != nil
+    }
+
+    private var shouldShowSaveButton: Bool {
+        (membershipImage != nil || croppedImage != nil) && (!cardInfo.isEmpty || !gymName.isEmpty)
+    }
+
+    private func editActionButtons() -> [ActionSheet.Button] {
+        [
+            .default(Text("Manual Crop")) { isManualCropping = true },
+            .default(Text("Update Card")) { showImagePicker = true },
+            .destructive(Text("Remove Card")) { showRemoveAlert = true },
+            .cancel()
+        ]
+    }
+
+    private func removeAlert() -> Alert {
+        Alert(
+            title: Text("Remove Membership Card"),
+            message: Text("Are you sure you want to remove your membership card?"),
+            primaryButton: .destructive(Text("Remove")) {
+                membershipImage = nil
+                croppedImage = nil
+                cardInfo = ""
+                gymName = ""
+                removeMembershipCardFromFirestore()
+            },
+            secondaryButton: .cancel()
+        )
     }
 
     func loadMembershipCardData() {
@@ -204,6 +243,10 @@ struct MembershipCardView: View {
 
             if let cardInfo = data["membershipCardInfo"] as? String {
                 self.cardInfo = cardInfo
+            }
+
+            if let gymName = data["gymName"] as? String {
+                self.gymName = gymName
             }
 
             if let imageUrl = data["membershipCardImageURL"] as? String {
@@ -254,6 +297,8 @@ struct MembershipCardView: View {
             return nil
         }
 
+        var croppedImage: UIImage? = nil
+        
         let request = VNDetectRectanglesRequest { request, error in
             if let error = error {
                 print("Error detecting rectangles: \(error.localizedDescription)")
@@ -286,6 +331,7 @@ struct MembershipCardView: View {
             try handler.perform([request])
         } catch {
             print("Error performing rectangle detection: \(error.localizedDescription)")
+            return nil
         }
 
         return croppedImage
@@ -374,6 +420,7 @@ struct MembershipCardView: View {
                 let db = Firestore.firestore()
                 db.collection("users").document(currentUserID).setData([
                     "membershipCardInfo": cardInfo,
+                    "gymName": gymName,
                     "membershipCardImageURL": downloadURL.absoluteString
                 ], merge: true) { error in
                     if let error = error {
@@ -397,6 +444,7 @@ struct MembershipCardView: View {
         let db = Firestore.firestore()
         db.collection("users").document(currentUserID).updateData([
             "membershipCardInfo": FieldValue.delete(),
+            "gymName": FieldValue.delete(),
             "membershipCardImageURL": FieldValue.delete()
         ]) { error in
             if let error = error {
@@ -404,6 +452,131 @@ struct MembershipCardView: View {
             } else {
                 print("Membership card data removed successfully")
             }
+        }
+    }
+}
+
+struct EditButton: View {
+    @Binding var showEditOptions: Bool
+
+    var body: some View {
+        Button(action: {
+            showEditOptions.toggle()
+        }) {
+            HStack {
+                Image(systemName: "pencil.circle")
+                Text("Edit Card Info")
+                    .fontWeight(.semibold)
+            }
+            .frame(maxWidth: .infinity) // Ensure full width
+            .padding()
+            .background(Color.accentColor)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+        }
+        .padding(.horizontal) // Padding between buttons
+    }
+}
+
+struct EmptyStateView: View {
+    @Binding var showImagePicker: Bool
+
+    var body: some View {
+        VStack(spacing: 16) {
+            Text("No Membership Card Uploaded")
+                .foregroundColor(.gray)
+                .padding()
+            
+            Button(action: {
+                showImagePicker.toggle()
+            }) {
+                Text("Upload Card")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct CardInfoView: View {
+    @Binding var cardInfo: String
+    @Binding var gymName: String
+    @Binding var isEditingInfo: Bool
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Card Information")
+                .font(.headline)
+                .padding(.top, 20)
+            
+            if !gymName.isEmpty {
+                Text("Gym Name: \(gymName)")
+                    .font(.custom("YourCustomFont", size: 16))
+            }
+
+            Text(cardInfo)
+                .font(.custom("YourCustomFont", size: 16))
+                .padding()
+
+            EditButton(showEditOptions: $isEditingInfo)
+                .sheet(isPresented: $isEditingInfo) {
+                    EditCardInfoView(cardInfo: $cardInfo, gymName: $gymName)
+                }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct SaveButton: View {
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            HStack {
+                Image(systemName: "tray.and.arrow.down")
+                Text("Save")
+                    .fontWeight(.semibold)
+            }
+            .frame(maxWidth: .infinity) // Ensure full width
+            .padding()
+            .background(Color(hex: "#FF5733"))
+            .foregroundColor(.white)
+            .cornerRadius(10)
+        }
+        .padding(.horizontal) // Padding between buttons
+    }
+}
+
+struct EditCardInfoView: View {
+    @Binding var cardInfo: String
+    @Binding var gymName: String
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 16) {
+                TextField("Enter card information", text: $cardInfo)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(8)
+                    .padding()
+
+                TextField("Enter gym name", text: $gymName)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(8)
+                    .padding()
+
+                Spacer()
+            }
+            .navigationBarTitle("Edit Card Info", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Done") {
+                presentationMode.wrappedValue.dismiss()
+            })
         }
     }
 }
